9e0a479c07e02c3713cd958ba8230e84ff67ec3b
Jonathan Sterling <jonsterling@me.com>
2011-03-20 19:36:12 -0700

    Documented autoprop.hpp (explained why this is a BFD!)

--- autoprop.hpp
+++ autoprop.hpp
@@ -14,7 +14,52 @@ namespace js
     static void cleanup(T* value);
   };
   
-  
+  // `autoprop` is a thin wrapper for pointers to Objective-C objects, to be
+  // used in the generation of properties. Take the following example:
+  //
+  //     @interface Dog : NSObject
+  //     @property (copy) NSString *name;
+  //     @end
+  //
+  //     @implementation Dog
+  //     @synthesize name;
+  //
+  //     - (void)dealloc {
+  //       [name release];
+  //       [super dealloc];
+  //     }
+  //
+  //     @end
+  //
+  // When `name` is synthesized, an instance variable `name` is generated, and
+  // two methods (`-setName:` and `-name`) are added onto `Dog`. `-setName:`
+  // will release the current `name`, copy the new one and assign it to the
+  // instance variable. The assignment semantics provided with the `@property`
+  // directive (`retain`, `copy` or `assign`) affect the way these are
+  // implemented. Now consider the following:
+  // 
+  //     @interface Dog : NSObject
+  //     @property js::autoprop<NSString,js::copy> name;
+  //     @end
+  //
+  //     @implementation Dog
+  //     @synthesize name;
+  //     @end
+  //
+  // There are a few differences:
+  //
+  // 1. The type of `name` is no longer `NSString*`, but
+  // `js::autoprop<NSString,js::copy>`.
+  // 2. The property is actually `assign` (that is, the Objective-C Runtime
+  // generates a setter with `assign`-semantics); `autoprop`, however, handles
+  // assignment internally using `copy`-semantics (as specificed by `js::copy`).
+  // 3. We didn't have to release `name` in `-dealloc`. *This is a big fucking
+  // deal.*
+  //
+  // The reason we don't have to clean up after ourselves is that Objective-C
+  // actually calls available destructors for C++ object instance variables
+  // in `-dealloc`. In our destructor, we release the referenced object (unless
+  // `js::assign` is specified).
   template <class T, retain_policy Rp = retain>
   struct autoprop
   {
