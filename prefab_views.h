b775b894169b639f095f7b1259688c33780b6a7a
Jonathan Sterling <jonsterling@me.com>
2011-03-25 20:49:26 -0700

    Document `js::defaults`

--- prefab_views.h
+++ prefab_views.h
@@ -3,5 +3,16 @@
 
 namespace js
 {
+  // Using `js::defaults`, the following:
+  //
+  //     js::defaults[@"dog"] = @"tucker";
+  //     id dog = js::defaults[@"dog"];
+  //
+  // is equivalent to this much more verbose code:
+  //
+  //     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  //     [defaults setValue:@"tucker" forKey:@"dog"];
+  //     id dog = [defaults valueForKey:@"dog"];
+  //
   static view<> defaults([NSUserDefaults standardUserDefaults]);
 }
